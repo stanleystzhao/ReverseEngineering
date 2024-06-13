f1136d16b803f106c0ca717cb5cf4dc2d74198a7
stanleystzhao <STWZhao@outlook.com>
2024-06-12 22:38:46 -0700

    A password check program to resverse engineer

---
+++ password.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int getPass(char* b) {
+    char pass[100];
+    FILE* f = fopen("password.txt", "r");
+    if (f == NULL) {
+        return 0;
+    }
+    fgets(pass, 100, f);
+    fclose(f);
+    return strcmp(b, pass) == 0;
+    
+}
+
+int main(int argc, char **argv) {
+    char buffer[100];
+    printf("Enter password: ");
+    scanf("%64s", buffer);
+    if (getPass(buffer)) {
+        printf("Access granted\n");
+    }
+    return 0;
+}
